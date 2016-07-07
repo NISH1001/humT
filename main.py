9608febaac8da50bad0b569bd94f3beffad4aecd
Pkoiralap <Pkoiralap@gmail.com>
2016-07-07 17:20:18 +0545

    multiple comparisions at once

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ import sys
 def process(hum):
     #setting input waveform
     hum = pitchToTimeseries(hum)
-    hum = normalize(hum)
     
     data = database.load()
     l = {}
@@ -25,17 +24,15 @@ def process(hum):
         original = data[alias]['pitches']
         #convert org to time series
         org = pitchToTimeseries(original)
-        #noramlize the time series value
-        org = normalize(org)
-        #plotting the hum and original track
-        timeseries(org,hum)
         
-        
+        if (len(hum) > len(org)):
+            hum, org = org, hum
+                
         '''iterations for sampling down the track
         set it by dividing the original by hum i.e divide original
         into hum sizes
         '''
-        divideFactor = 0.95 * len(org)/len(hum)
+        divideFactor = 1.0 * len(org)/len(hum)
         
         #chunk size to divide the iterations into
         chunkSize = int(len(org)/divideFactor)
@@ -45,26 +42,59 @@ def process(hum):
         if iterations < 2:
             iterations = 2
         
+        #dictionary for path ,distances pair
+        pathDistancePairs = {}
+
+        chunkStart = 0
+        chunkEnd = chunkStart + chunkSize 
         
-        #list of distances
-        distances = []
-        for i in range(0,iterations):
-            chunkStart = int(len(org)/divideFactor*(i))
-            chunkEnd = chunkStart + chunkSize 
-            #sampling the org into chunks from chunkStart to chunkEnd
-            org_temp = org[chunkStart:chunkEnd]
-            #FastDTW used from the module
-            distance,path = fastdtw(hum,org_temp,dist = euc)
             
-            #plot similarities between the org_temp and hummed tune
-            similarityPlot(hum,org_temp,path)
-            #create a list of distances to compare later
-            distances += [distance]
+        while chunkEnd < len(org):
+            #sampling the org into chunks from chunkStart to chunkEnd
+            org_temp =  normalize(org[chunkStart:chunkEnd])
+            #FastDTW used from the module
+            distance,path = fastdtw(normalize(hum),org_temp,dist = euc)
+            
+            chunkStart += 50
+            chunkEnd = chunkStart + chunkSize 
+            #create a dictionary of type {key = path, value = distance} 
+            pathDistancePairs[distance] = path,org_temp
+
         #the min distances of all the distances is taken as the distance 
-        l[alias] = min(distances)
+        if pathDistancePairs == {}:
+            print("NULL pathDistancePair")
+        shortestDistance = min(d for d,value in pathDistancePairs.iteritems())
+
+        pathChunkPairs = pathDistancePairs[shortestDistance] 
+        #shortest path corresponding to minimum distance
+        shortestPath = pathChunkPairs[0]
         
+        #best chunk match
+        matchedChunk = pathChunkPairs[1]
+
+        l[alias] = shortestDistance,matchedChunk,shortestPath
+        print(alias,shortestDistance, " Finished comparing...")
+    
+    #showing results
+    results(l,normalize(hum))   
+
+def results(l,hum):
+    #shortest Distance among the many distances
+    shortestDistance = min(l[alias][0] for alias in l)
     for alias in l:
-        print(alias, l[alias])
+        if l[alias][0] == shortestDistance:
+            print(alias,shortestDistance)
+            break
+    #key corresponding to the minimum distance
+    #key = [k for k in l if l[k][0] == shortestDistance]
+    for k in l:
+        if l[k][0] == shortestDistance:
+            matchedChunk = l[k][1]
+            shortestPath = l[k][2]
+    #ploting similarity plot 
+    #timeseries(hum,matchedChunk)
+    similarityPlot(hum,matchedChunk,shortestPath)
+
 
 def main():
     #inputName for argv
