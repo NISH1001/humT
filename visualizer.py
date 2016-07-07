223312d5c43d8b775a12ae68522f6bd386d27eca
Pkoiralap <Pkoiralap@gmail.com>
2016-07-07 10:38:04 +0545

    timeseries conversion, multiple plot and similarity plot addition

--- visualizer.py
+++ visualizer.py
@@ -4,13 +4,37 @@ from matplotlib import collections as matcollect
 import numpy as np
 import itertools
 
-def timeseries(l):
-    pitches = l
+#convert the pitch data to timeseries data
+def pitchToTimeseries(l):
     pitchdata = []
-    for p in range(0,len(pitches),2):
-       pitchdata += [pitches[p]] * pitches[p+1]
-    plt.plot(pitchdata)
+    
+    #convert (p1,t1,p2,t2....) into (p1,p1,....t1 p1s, p2 ,p2,p2..... t2 p2s...)
+    for p in range(0,len(l)-1,2):
+       pitchdata += [l[p]] * int(l[p+1])
+    return pitchdata
+
+
+#use like timeseries(a,b,c,d,.....)
+def timeseries(*arg):
+    for data in arg:
+        plt.plot(data)
     plt.show()
+
+#shows the similarity plot between hum and original
+def similarityPlot(hum,org,path):
+    #plotting the similarity
+    plt.plot(hum,'b',label='Hummed Tune')
+    plt.plot(org,'g',label='Original Tune')
+    i = 1   
+    for [map_x,map_y] in path:
+        #for every 50 paths show only 1
+        if i % 50 == 0:
+            plt.plot([map_x,map_y],[hum[map_x],org[map_y]],'r')
+            i = 1
+        else:
+            i = i+1
+    plt.show()
+
 
 def main():
     pass
