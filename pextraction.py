40ea2b8b4f1eb609a8f932d59ecf601d9fbed214
Pkoiralap <Pkoiralap@gmail.com>
2016-07-07 10:36:50 +0545

    absolute pitch conversion to musical notes index

--- pextraction.py
+++ pextraction.py
@@ -1,6 +1,12 @@
 import os
 from math import sin,pi
 
+notePitches =   [   61.74,65.41,69.30,73.42,77.78,82.41,87.31,92.50,98.00,103.83,110.00,116.54,123.47,130.81,
+                    138.59,146.83,155.56,164.81,174.61,185.00,196.00,207.65,220.00,233.08,246.94,
+                    261.63,277.18,293.66,311.13,329.63,349.23 ,369.99,392.00,415.30,440.00,466.16,493.88,
+                    523.25,554.37,587.33,622.25, 659.25,698.46,739.99 ,783.99,830.61,880.00
+                ]
+    
 def track_pitch(inputFilename): 
     #Load the Aubiopitch from command and save the data onto a file
     os.system("aubiopitch -i" + inputFilename+ " -r 44100 -l 0.8 > temp.txt")
@@ -45,7 +51,17 @@ def track_pitch(inputFilename):
             temp = []
             prev_pitch = pitch
     
-    return pitches
+    #to use pitch slope method for comparision, to-do 
+    #pitchSlope = []
+    currPitch = []
+    for p in range(0,len(pitches)-2,2):
+        #converting the pitches value to the index of the notePitches
+        #This is like mapping the frequencies to the musical notes
+        currPitch += [min(range(len(notePitches)),key = lambda i: abs(notePitches[i]- pitches[p])), pitches[p+1]]
+        
+        #pitchSlope += [abs(currPitch - nextPitch)/pitchLen]
+    print(currPitch)
+    return currPitch
 
 def main():
     pass
