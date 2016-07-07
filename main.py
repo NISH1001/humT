ea5b9062c4281ebfa74a9091c19778ddd044aa06
Pkoiralap <Pkoiralap@gmail.com>
2016-07-07 10:36:20 +0545

    system arguments options addition and sampling down original song

--- main.py
+++ main.py
@@ -1,46 +1,93 @@
 #!/usr/bin/env python3
 
-from processor import normalize, dtw, euc
+#sources import
+from processor import normalize,euc
 from pextraction import track_pitch
-from visualizer import timeseries
+from visualizer import timeseries,pitchToTimeseries,similarityPlot
 from recorder import Recorder
 import database
 
+#inbuilt imports
+from fastdtw import fastdtw
+import matplotlib.pyplot as plt
+import sys
+
 def process(hum):
-    hum_normalized = normalize(hum)
+    #setting input waveform
+    hum = pitchToTimeseries(hum)
+    hum = normalize(hum)
+    
     data = database.load()
     l = {}
+    
     for alias in data:
+        #setting input waveform
         original = data[alias]['pitches']
-        normalized = normalize(original)
-        dtwdist, dtwpath = dtw(hum_normalized, normalized, euc, 14)
-        l[alias] = (dtwdist, dtwpath)
-
+        #convert org to time series
+        org = pitchToTimeseries(original)
+        #noramlize the time series value
+        org = normalize(org)
+        #plotting the hum and original track
+        timeseries(org,hum)
+        
+        
+        '''iterations for sampling down the track
+        set it by dividing the original by hum i.e divide original
+        into hum sizes
+        '''
+        divideFactor = 0.95 * len(org)/len(hum)
+        
+        #chunk size to divide the iterations into
+        chunkSize = int(len(org)/divideFactor)
+        
+        #atleast 2 iterations 
+        iterations = int(divideFactor+0.5)
+        if iterations < 2:
+            iterations = 2
+        
+        
+        #list of distances
+        distances = []
+        for i in range(0,iterations):
+            chunkStart = int(len(org)/divideFactor*(i))
+            chunkEnd = chunkStart + chunkSize 
+            #sampling the org into chunks from chunkStart to chunkEnd
+            org_temp = org[chunkStart:chunkEnd]
+            #FastDTW used from the module
+            distance,path = fastdtw(hum,org_temp,dist = euc)
+            
+            #plot similarities between the org_temp and hummed tune
+            similarityPlot(hum,org_temp,path)
+            #create a list of distances to compare later
+            distances += [distance]
+        #the min distances of all the distances is taken as the distance 
+        l[alias] = min(distances)
+        
     for alias in l:
-        print(alias, l[alias][0])
+        print(alias, l[alias])
 
 def main():
-    # start recording -> 5 sec default
-    recorder = Recorder()
-    frames = recorder.start(5)
-    recorder.write_wav(frames, "input.wav")
+    #inputName for argv
+    inputName = ""
+    
+    
+    #if the input is record
+    if sys.argv[1] == 'record':
+        # start recording -> 5 sec default
+        recorder = Recorder()
+        frames = recorder.start(5)
+        recorder.write_wav(frames, "input.wav")
+        inputName = "input.wav"
+
+    #else if it is a filename
+    else:
+        inputName = sys.argv[1]
 
     # load the hum and process
-    hum = track_pitch("input.wav")
-    timeseries(hum)
+    hum = track_pitch(inputName)
+    
+    #process the hum and compare with the database
     process(hum)
-
-    """
-    print("hum vs original")
-    x = normalize(x)
-    dtwdist, dtwpath = dtw(x, z, euc, 14)
-    print(dtwdist)
-
-    y = normalize(track_pitch("notNearOriginal.wav"))
-    print("hum vs non-original")
-    dtwdist, dtwpath = dtw(x, y, euc, 14)
-    print(dtwdist)
-    """
 
 if __name__ == "__main__":
     main()
