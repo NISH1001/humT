49e55d77a2a5e6e8ccd4a6541b81873f4b7fc0a2
Pkoiralap <Pkoiralap@gmail.com>
2016-08-17 07:42:54 +0545

    Timeseries and pitchToTimeseries modification

--- visualizer.py
+++ visualizer.py
@@ -10,21 +10,26 @@ def pitchToTimeseries(l):
     
     #convert (p1,t1,p2,t2....) into (p1,p1,....t1 p1s, p2 ,p2,p2..... t2 p2s...)
     for p in range(0,len(l)-1,2):
-       pitchdata += [l[p]] * int(l[p+1])
+       pitchdata += [l[p]] * (int(l[p+1]/2))
     return pitchdata
 
 
-#use like timeseries(a,b,c,d,.....)
+#use like timeseries(a,"a legend" ,b,"b legend",.....)
 def timeseries(*arg):
-    for data in arg:
-        plt.plot(data)
+    legend_list = []
+    for i in range(1,len(arg),2):
+        plt.plot(arg[i-1])
+        legend_list.append(arg[i])
+    
+    plt.legend(legend_list)
     plt.show()
 
 #shows the similarity plot between hum and original
 def similarityPlot(hum,org,path,jump):
     #plotting the similarity
-    plt.plot(hum,'b',label='Hummed Tune')
-    plt.plot(org,'g',label='Original Tune')
+    plt.plot(hum)
+    plt.plot(org)
+    
     i = 1   
     for [map_x,map_y] in path:
         #for every jump value of paths show only 1
@@ -35,6 +40,8 @@ def similarityPlot(hum,org,path,jump):
             i = 1
         else:
             i = i+1
+    
+    plt.legend(['Hummed tune','Original tune','Similarity'])
     plt.show()
 
 
